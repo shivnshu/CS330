14b06118b2a0c57bf148e9633ee83903dc29c73f
Shivanshu Singh <shivnshus@gmail.com>
2018-10-05 18:25:50 +0530

    Implement part(a) of assignment 3

--- Assignments/assignment3/user/init.c
+++ Assignments/assignment3/user/init.c
@@ -1,4 +1,5 @@
 #include<init.h>
+#include<lib.h>
 static void exit(int);
 static int main(void);
 
@@ -56,17 +57,49 @@ static long write(char *ptr, int size)
    return(_syscall2(SYSCALL_WRITE, (u64)ptr, size));
 }
 
+static long signal(int signo, unsigned long handler)
+{
+    return(_syscall2(SYSCALL_SIGNAL, signo, handler));
+}
+
+void signal_handler()
+{
+    /*asm volatile ("pushq %rax;"*/
+                  /*"pushq %rbx;"*/
+                  /*"pushq %rcx;"*/
+                  /*"pushq %rdx;"*/
+                  /*"pushq %rsi;"*/
+                  /*"pushq %rdi;"*/
+                  /*"pushq %r8;"*/
+                  /*"pushq %r9;"*/
+                  /*"pushq %r10;"*/
+                  /*"pushq %r11;"*/
+                  /*"pushq %r12;"*/
+                  /*"pushq %r13;"*/
+                  /*"pushq %r14;"*/
+                  /*"pushq %r15;");*/
+    write("In custom signal handler\n", 25);
+    printf("In custom signal handler by printf\n");
+    /*asm volatile ("popq %r15;"*/
+                  /*"popq %r14;"*/
+                  /*"popq %r13;"*/
+                  /*"popq %r12;"*/
+                  /*"popq %r11;"*/
+                  /*"popq %r10;"*/
+                  /*"popq %r9;"*/
+                  /*"popq %r8;"*/
+                  /*"popq %rdi;"*/
+                  /*"popq %rsi;"*/
+                  /*"popq %rdx;"*/
+                  /*"popq %rcx;"*/
+                  /*"popq %rbx;"*/
+                  /*"popq %rax;");*/
+    exit(0);
+}
+
 static int main()
 {
-  unsigned long i, j;
-  unsigned long buff[4096];
-  i = getpid();
- 
-  for(i=0; i<4096; ++i){
-      j = buff[i];
-  }
-  i=0x100034;
-  j = i / (i-0x100034);
-  exit(-5);
-  return 0;
+    signal(1, (unsigned long)signal_handler);
+    int i = 10 / (10 - 10);
+    return 0;
 }
