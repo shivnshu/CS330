4ac902e561e6490cebfb8ed4799f5942be28e4c1
Shivanshu Singh <shivnshus@gmail.com>
2018-11-15 00:40:34 +0530

    Add more tests

---
+++ Assignments/assignment4/test/find_one_and_read.c
@@ -0,0 +1,15 @@
+#include "kvstore.h"
+int main(int argc, char **argv)
+{
+  if (argv[1]) {
+    char value[4096];
+    if(get_key(argv[1], value) < 0) {
+      printf("Get error for key = %s\n", argv[1]);
+    } else {
+      printf("For key = %s value=%s\n", argv[1], value);
+    }
+  } else {
+    printf("Usage: %s <key>\n", argv[0]);
+  }
+
+}
